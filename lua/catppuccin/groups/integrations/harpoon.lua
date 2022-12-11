cf5c7076f29cac0408b1a6a04baf8c61d8fd1051
Anthony Bennett <goodoneanthony@gmail.com>
2022-12-11 12:58:29 -0800

    feat: updated harpoon highlights

--- lua/catppuccin/groups/integrations/harpoon.lua
+++ lua/catppuccin/groups/integrations/harpoon.lua
@@ -1,10 +1,10 @@
 local M = {}
 
 function M.get()
-	return {
-		HarpoonWindow = { fg = C.text, bg = O.transparent_background and C.none or C.base },
-		HarpoonBorder = { fg = C.blue },
-	}
+  return {
+    HarpoonWindow = { fg = C.text, bg = O.transparent_background and C.none or C.crust },
+    HarpoonBorder = { fg = C.crust, bg = O.transparent_background and C.none or C.crust },
+  }
 end
 
 return M
